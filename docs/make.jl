35f664191659294080863214194e5ed70afbdda4
Benedict Geihe <bgeihe@uni-koeln.de>
2024-07-15 17:33:49 +0200

    format

--- docs/make.jl
+++ docs/make.jl
@@ -12,19 +12,18 @@ DocMeta.setdocmeta!(TrixiAtmo, :DocTestSetup, :(using TrixiAtmo);
 makedocs(;
          modules = [TrixiAtmo],
          authors = "Benedict Geihe <bgeihe@uni-koeln.de>, Tristan Montoya <montoya.tristan@gmail.com, Hendrik Ranocha <hendrik.ranocha@uni-mainz.de>, Michael Schlottke-Lakemper <michael@sloede.com>",
-         repo = Remotes.GitHub("trixi-framework", "TrixiAtmo.jl/blob/{commit}{path}#{line}"),
+         repo = Remotes.GitHub("trixi-framework",
+                               "TrixiAtmo.jl/blob/{commit}{path}#{line}"),
          sitename = "TrixiAtmo.jl",
          format = Documenter.HTML(;
                                   prettyurls = get(ENV, "CI", "false") == "true",
                                   canonical = "https://trixi-framework.github.io/TrixiAtmo.jl",
                                   edit_link = "main",
                                   assets = String[],),
-         pages = [
-             "Home" => "index.md",
-         ],
+         pages = ["Home" => "index.md"],
          plugins = [links],)
 
 deploydocs(;
            repo = "github.com/trixi-framework/TrixiAtmo.jl",
            devbranch = "main",
-           push_preview = false)
+           push_preview = "false")
