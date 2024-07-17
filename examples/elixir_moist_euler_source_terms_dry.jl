92001fa7720b88cfdff456f448b82a023ffbdc86
Arpit-Babbar <arpitbabbar@gmail.com>
2024-07-17 11:23:11 +0530

    Formatter

--- examples/elixir_moist_euler_source_terms_dry.jl
+++ examples/elixir_moist_euler_source_terms_dry.jl
@@ -8,11 +8,11 @@ using TrixiAtmo: source_terms_convergence_test_dry, initial_condition_convergenc
 ###############################################################################
 # semidiscretization of the compressible Euler equations
 
-equations = CompressibleMoistEulerEquations2D( ;g=0.0) 
+equations = CompressibleMoistEulerEquations2D(; g = 0.0)
 
 initial_condition = initial_condition_convergence_test_dry
 
-solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs)
+solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
 coordinates_min = (0.0, 0.0)
 coordinates_max = (2.0, 2.0)
@@ -21,10 +21,8 @@ cells_per_dimension = (16, 16)
 
 mesh = StructuredMesh(cells_per_dimension, coordinates_min, coordinates_max)
 
-
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
-                                    source_terms=source_terms_convergence_test_dry)
-
+                                    source_terms = source_terms_convergence_test_dry)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
@@ -35,16 +33,16 @@ ode = semidiscretize(semi, tspan)
 summary_callback = SummaryCallback()
 
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
-alive_callback = AliveCallback(analysis_interval=analysis_interval)
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
 
-save_solution = SaveSolutionCallback(interval=100,
-                                     save_initial_solution=true,
-                                     save_final_solution=true,
-                                     solution_variables=cons2prim)
+save_solution = SaveSolutionCallback(interval = 100,
+                                     save_initial_solution = true,
+                                     save_final_solution = true,
+                                     solution_variables = cons2prim)
 
-stepsize_callback = StepsizeCallback(cfl=1.0)
+stepsize_callback = StepsizeCallback(cfl = 1.0)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback, alive_callback,
@@ -54,7 +52,7 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-            save_everystep=false, callback=callbacks);
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+            dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false, callback = callbacks);
 summary_callback() # print the timer summary
--- examples/elixir_moist_euler_source_terms_moist.jl
+++ examples/elixir_moist_euler_source_terms_moist.jl
@@ -3,12 +3,13 @@
 
 using OrdinaryDiffEq
 using Trixi, TrixiAtmo
-using TrixiAtmo: source_terms_convergence_test_moist, initial_condition_convergence_test_moist
+using TrixiAtmo: source_terms_convergence_test_moist,
+                 initial_condition_convergence_test_moist
 
 ###############################################################################
 # semidiscretization of the compressible Euler equations
 
-equations = CompressibleMoistEulerEquations2D() 
+equations = CompressibleMoistEulerEquations2D()
 
 initial_condition = initial_condition_convergence_test_moist
 
@@ -26,10 +27,8 @@ cells_per_dimension = (4, 4)
 
 mesh = StructuredMesh(cells_per_dimension, coordinates_min, coordinates_max)
 
-
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
-                                    source_terms=source_terms_convergence_test_moist)
-
+                                    source_terms = source_terms_convergence_test_moist)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
@@ -40,16 +39,16 @@ ode = semidiscretize(semi, tspan)
 summary_callback = SummaryCallback()
 
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
-alive_callback = AliveCallback(analysis_interval=analysis_interval)
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
 
-save_solution = SaveSolutionCallback(interval=100,
-                                     save_initial_solution=true,
-                                     save_final_solution=true,
-                                     solution_variables=cons2prim)
+save_solution = SaveSolutionCallback(interval = 100,
+                                     save_initial_solution = true,
+                                     save_final_solution = true,
+                                     solution_variables = cons2prim)
 
-stepsize_callback = StepsizeCallback(cfl=1.0)
+stepsize_callback = StepsizeCallback(cfl = 1.0)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback, alive_callback,
@@ -59,7 +58,7 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-            save_everystep=false, callback=callbacks);
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+            dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false, callback = callbacks);
 summary_callback() # print the timer summary
