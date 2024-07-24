bc9bb5b8eceeb79219b0532a8d4fa0d4599071e8
FnHck <95546963+FnHck@users.noreply.github.com>
2024-07-24 18:25:25 +0200

    updating equations

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -1,1 +1,9 @@
+using Trixi: AbstractEquations
 
+abstract type AbstractCompressibleMoistEulerEquations{NDIMS, NVARS} <:
+              AbstractEquations{NDIMS, NVARS} end
+
+abstract type AbstractCompressibleRainyEulerEquations{NDIMS, NVARS} <:
+              AbstractEquations{NDIMS, NVARS} end
+
+include("compressible_rainy_euler_2d.jl")
