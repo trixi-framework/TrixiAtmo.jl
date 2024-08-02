118b06d3f8078617341c1fcfb23afa8be54eeb03
FnHck <95546963+FnHck@users.noreply.github.com>
2024-08-02 17:04:15 +0200

    rename and partial implementation of initial condition

--- examples/elixir_rainy_euler_bubble_no_phase_change.jl
+++ examples/elixir_rainy_euler_bubble_no_phase_change.jl
@@ -1,12 +1,37 @@
 using OrdinaryDiffEq
 using Trixi
 using TrixiAtmo
-using TrixiAtmo: source_terms_no_rain
+using TrixiAtmo: source_terms_no_phase_change
 
 
 
-function initial_condition_bubble_no_rain(x, t, equations::CompressibleRainyEulerEquations2D)
+###  Bryan-Fritsch Moist Benchmark similar to Example 4.2 of:
+# Sabine Doppler, Philip L. Lederer, Joachim Schöberl, Henry von Wahl,
+# A discontinuous Galerkin approach for atmospheric flows with implicit condensation,
+# Journal of Computational Physics,
+# Volume 499,
+# 2024,
+# 112713,
+# ISSN 0021-9991
+
+function initial_condition_bubble_bryan_fritsch(x, t, equations::CompressibleRainyEulerEquations2D) 
+    # Position of the bubble defined by center and radius:
+    x_center = 10_000.0
+    z_center =  2_000.0
+    
+    x_radius =  2_000.0
+    z_radius =  2_000.0
+
+    # Hydrostatic Base State (compare Doppler et al. Appendix B.2)
+    q_w      =     0.02               # total water fraction
+    theta_e  =    320.0               # wet equivalent potential temperature
+    
+    p_ref = equations.ref_pressure
+
+    L = min(sqrt(((x[1] - x_center) / x_radius)^2 + ((x[2] - z_center) / z_radius)^2), 1.0)
+
     #TODO
+
     return SVector(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 end
 
@@ -29,22 +54,20 @@ surface_flux = flux_lax_friedrichs
 
 solver = DGSEM(basis, surface_flux)
 
-#TODO adjust values
-coordinates_min = (0.0, 0.0)
-coordinates_max = (1.0, 1.0)
+coordinates_min = (     0.0,      0.0)
+coordinates_max = (20_000.0, 10_000.0)
 
 cells_per_dimension = (64, 32)
 mesh = StructuredMesh(cells_per_dimension, coordinates_min, coordinates_max,
                       periodicity = (true, false))
 
-semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_bubble_no_rain, solver,
-                                    source_terms = source_terms_no_rain,
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_bubble_bryan_fritsch, solver,
+                                    source_terms = source_terms_no_phase_change,
                                     boundary_conditions = boundary_conditions)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-#TODO adjust values
 tspan = (0.0, 1000.0)
 
 ode = semidiscretize(semi, tspan)
