76653b14e7e6a4ddb4e5c824c45c11c9366bb067
FnHck <95546963+FnHck@users.noreply.github.com>
2024-08-12 16:51:42 +0200

    most simple test case I could find

---
+++ examples/elixir_rainy_euler_bubble_dry_(basic_test).jl
@@ -0,0 +1,120 @@
+using OrdinaryDiffEq
+using Trixi
+using TrixiAtmo
+using TrixiAtmo: source_terms_no_phase_change
+
+
+
+# copied from elixir_euler_warm_bubble.jl for quick tests
+function initial_condition_bubble_dry(x, t, equations::CompressibleRainyEulerEquations2D)
+    g   = equations.gravity
+    c_v = equations.c_dry_air_const_volume
+    c_p = equations.c_dry_air_const_pressure
+
+    # center of perturbation
+    center_x = 10000.0
+    center_z = 2000.0
+    # radius of perturbation
+    radius = 2000.0
+    # distance of current x to center of perturbation
+    r = sqrt((x[1] - center_x)^2 + (x[2] - center_z)^2)
+
+    # perturbation in potential temperature
+    potential_temperature_ref = 273.15
+    potential_temperature_perturbation = 0.0
+    if r <= radius
+        potential_temperature_perturbation = 2 * cospi(0.5 * r / radius)^2
+    end
+    potential_temperature = potential_temperature_ref + potential_temperature_perturbation
+
+    # Exner pressure, solves hydrostatic equation for x[2]
+    exner = 1 - g / (c_p * potential_temperature) * x[2]
+
+    # pressure
+    p_0 = 100_000.0  # reference pressure
+    R = c_p - c_v    # gas constant (dry air)
+    p = p_0 * exner^(c_p / R)
+
+    # temperature
+    T = potential_temperature * exner
+
+    # density
+    rho = p / (R * T)
+
+    v1 = 0.0
+    v2 = 0.0
+    E = c_v * T + 0.5 * (v1^2 + v2^2)
+
+    # random experiments
+    return SVector(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, T, rho, 0.0, 0.0, E, 0.0, potential_temperature)
+end
+
+
+
+###############################################################################
+# semidiscretization of the compressible rainy Euler equations
+
+equations = CompressibleRainyEulerEquations2D()
+
+boundary_conditions = (x_neg = boundary_condition_periodic,
+                       x_pos = boundary_condition_periodic,
+                       y_neg = boundary_condition_slip_wall,
+                       y_pos = boundary_condition_slip_wall)
+
+polydeg = 3
+basis = LobattoLegendreBasis(polydeg)
+
+surface_flux = flux_lax_friedrichs
+
+solver = DGSEM(basis, surface_flux)
+
+coordinates_min = (     0.0,      0.0)
+coordinates_max = (20_000.0, 10_000.0)
+
+cells_per_dimension = (64, 32)
+mesh = StructuredMesh(cells_per_dimension, coordinates_min, coordinates_max,
+                      periodicity = (true, false))
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_bubble_dry, solver,
+                                    source_terms = source_terms_no_phase_change,
+                                    boundary_conditions = boundary_conditions)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 1000.0)
+
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 1000
+
+# entropy?
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval,
+                                     extra_analysis_errors = (:entropy_conservation_error,))
+
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
+
+save_solution = SaveSolutionCallback(interval = analysis_interval,
+                                     save_initial_solution = true,
+                                     save_final_solution = true,
+                                     output_directory = "out",
+                                     solution_variables = cons2prim)
+
+stepsize_callback = StepsizeCallback(cfl = 1.0)
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback,
+                        alive_callback,
+                        save_solution,
+                        stepsize_callback)
+
+###############################################################################
+# run the simulation
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+            maxiters = 1.0e7,
+            dt = 1.0, 
+            save_everystep = false, callback = callbacks);
+
+summary_callback()
