da79bbc581b8dd2931075a4cf36d525866759b81
Tristan Montoya <montoya.tristan@gmail.com>
2024-08-16 10:11:35 +0200

    added missing prefixes for non-exported Trixi functions and fixed signature in SemidiscretizationHyperbolic constructor

--- src/solvers/dgsem_p4est/dg.jl
+++ src/solvers/dgsem_p4est/dg.jl
@@ -1,7 +1,7 @@
 # Extract contravariant vector Ja^i (i = index) as SVector
 # This function dispatches on the type of contravariant_vectors
 static2val(::Trixi.StaticInt{N}) where {N} = Val{N}()
-@inline function get_contravariant_vector(index, contravariant_vectors::PtrArray,
+@inline function Trixi.get_contravariant_vector(index, contravariant_vectors::PtrArray,
                                           indices...)
     SVector(ntuple(@inline(dim->contravariant_vectors[dim, index, indices...]),
                    static2val(static_size(contravariant_vectors, Trixi.StaticInt(1)))))
