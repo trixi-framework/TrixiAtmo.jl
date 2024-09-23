a229219ee1bf55bde7f7247d48a053d1ed128541
Marco Artiano <artianomarco97@gmail.com>
2024-09-23 16:36:17 +0200

    minor changes

--- examples/elixir_moist_euler_dry_bubble.jl
+++ examples/elixir_moist_euler_dry_bubble.jl
@@ -61,7 +61,7 @@ source_term = source_terms_geopotential
 polydeg = 4
 basis = LobattoLegendreBasis(polydeg)
 
-surface_flux = flux_LMARS(340.0)
+surface_flux = flux_LMARS(360.0)
 volume_flux = flux_chandrashekar
 
 volume_integral = VolumeIntegralFluxDifferencing(volume_flux)
