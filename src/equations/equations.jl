a820c9781f8584dfecaaa9d146544ee31b92e6cf
Marco Artiano <artianomarco97@gmail.com>
2024-09-24 21:59:32 +0200

    deleted useless parts

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -5,10 +5,5 @@ abstract type AbstractCompressibleMoistEulerEquations{NDIMS, NVARS} <:
 abstract type AbstractCompressibleEulerPotentialTemperatureEquations{NDIMS, NVARS} <:
               AbstractEquations{NDIMS, NVARS} end
 
-# struct flux_LMARS{SpeedOfSound}
-#     # Estimate for the speed of sound
-#     speed_of_sound::SpeedOfSound
-# end
-
 include("compressible_moist_euler_2d_lucas.jl")
 include("compressible_euler_potential_temperature_2d.jl")
