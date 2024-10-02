602d7e5d84540ce00b9fa62bf09a1ff2e5839689
Benedict Geihe <bgeihe@uni-koeln.de>
2024-10-02 16:15:15 +0200

    add {false} to explicitly indicate out-of-place

--- src/callbacks_stage/nonlinear_solve_dg.jl
+++ src/callbacks_stage/nonlinear_solve_dg.jl
@@ -3,23 +3,24 @@ using NonlinearSolve
 
 @muladd begin
 struct NonlinearSolveDG
-    nonlin_fct
+    nonlin_fct::NonlinearFunction{false}
     variables_index_vector
     tolerance::Float64
 end
 
 function NonlinearSolveDG(residual, jacobian, variables_index_vector, tolerance)
-    nonlin_fct = NonlinearFunction(residual; jac = jacobian)
+    nonlin_fct = NonlinearFunction{false}(residual; jac = jacobian)
     return NonlinearSolveDG(nonlin_fct, variables_index_vector, tolerance)
 end
 
 function (limiter!::NonlinearSolveDG)(u_ode, integrator,
                                         semi::AbstractSemidiscretization, t)
     u = wrap_array(u_ode, semi)
+    _, equations, solver, cache = Trixi.mesh_equations_solver_cache(semi)
+    @unpack nonlin_fct, variables_index_vector, tolerance = limiter!
 
     @trixi_timeit timer() "nonlinear system solver" begin
-        nonlinear_solve_dg_2d!(u, limiter!.nonlin_fct, limiter!.variables_index_vector,
-                               limiter!.tolerance, semi.equations, semi.solver, semi.cache)
+        nonlinear_solve_dg_2d!(u, nonlin_fct, variables_index_vector, tolerance, equations, solver, cache)
     end
 end
 
--- src/callbacks_stage/nonlinear_solve_dg2d.jl
+++ src/callbacks_stage/nonlinear_solve_dg2d.jl
@@ -2,15 +2,18 @@ using NonlinearSolve
 using Trixi: get_node_vars
 
 @muladd begin
-function nonlinear_solve_dg_2d!(u, nonlin_fct, variables_index_vector,
+function nonlinear_solve_dg_2d!(u, nonlin_fct::NonlinearFunction{false},
+                                variables_index_vector,
                                 tolerance, equations, dg::DGSEM, cache)
     for element in eachelement(dg, cache)
         for j in eachnode(dg), i in eachnode(dg)
             u_node = get_node_vars(u, equations, dg, i, j, element)
             initial_guess = u_node[variables_index_vector]
-            prob = NonlinearProblem(nonlin_fct, initial_guess, (u_node, equations))
+            prob = NonlinearProblem{false}(nonlin_fct, initial_guess,
+                                            (u_node, equations))
             # TODO: abs and rel?
-            res = solve(prob, SimpleNewtonRaphson(); abstol = tolerance, reltol = tolerance)
+            res = solve(prob, SimpleNewtonRaphson(); abstol = tolerance,
+                        reltol = tolerance)
 
             # similar to set_node_vars!
             for index in eachindex(variables_index_vector)
