fb379e040304ddeac63ca227e07c090f8e70a571
Tristan Montoya <montoya.tristan@gmail.com>
2024-11-04 14:55:51 +0100

    no need to use Float32 for physical constant defaults

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -1,10 +1,10 @@
 using Trixi: AbstractEquations
 
 # Physical constants
-const EARTH_RADIUS = 6.37122f6  # m
-const EARTH_GRAVITATIONAL_ACCELERATION = 9.80616f0  # m/s²
-const EARTH_ROTATION_RATE = 7.292f-5  # rad/s
-const SECONDS_PER_DAY = 8.64f4
+const EARTH_RADIUS = 6.37122  # m
+const EARTH_GRAVITATIONAL_ACCELERATION = 9.80616  # m/s²
+const EARTH_ROTATION_RATE = 7.292e-5  # rad/s
+const SECONDS_PER_DAY = 8.64e4
 
 @doc raw"""
     AbstractCovariantEquations{NDIMS, 
