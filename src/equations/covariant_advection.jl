e30025e803729c14cfa03f175d0ef82f98ab6a09
Tristan Montoya <montoya.tristan@gmail.com>
2024-12-06 16:57:32 +0100

    Apply suggestions from code review
    Co-authored-by: Andrés Rueda-Ramírez <aruedara@uni-koeln.de>

--- src/equations/covariant_advection.jl
+++ src/equations/covariant_advection.jl
@@ -89,7 +89,7 @@ end
     return SVector(u[1], z, z)
 end
 
-# Numerical flux plus dissipation for abstract covariant equations as a function of the 
+# Flux for abstract covariant equations as a function of the 
 # state vector u, as well as the auxiliary variables aux_vars, which contain the geometric 
 # information required for the covariant form
 @inline function Trixi.flux(u, aux_vars, orientation::Integer,
