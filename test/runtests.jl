f22b9298ab0fc02c3e9d197fdfd703b5226ddc46
Benedict Geihe <bgeihe@uni-koeln.de>
2025-04-16 10:13:45 +0200

    use TRIXI_TEST

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,29 +4,28 @@ using Test
 # variable to determine the subset of tests to execute.
 #
 # We could do the same once we have a lot of tests
-const TRIXIATMO_TEST = get(ENV, "TRIXIATMO_TEST", "all")
-const TRIXI_TEST = get(ENV, "TRIXI_TEST", "none")
-const TRIXIATMO_MPI_NPROCS = clamp(Sys.CPU_THREADS, 2, 3)
-const TRIXIATMO_NTHREADS = clamp(Sys.CPU_THREADS, 2, 3)
+const TRIXI_TEST = get(ENV, "TRIXI_TEST", "all")
+const TRIXI_MPI_NPROCS = clamp(Sys.CPU_THREADS, 2, 3)
+const TRIXI_NTHREADS = clamp(Sys.CPU_THREADS, 2, 3)
 
 @time @testset verbose=true showtiming=true "TrixiAtmo.jl tests" begin
-    @time if TRIXIATMO_TEST == "all" || TRIXIATMO_TEST == "trixi_consistency"
+    @time if TRIXI_TEST == "all" || TRIXI_TEST == "trixi_consistency"
         include("test_trixi_consistency.jl")
     end
 
-    @time if TRIXIATMO_TEST == "all" || TRIXIATMO_TEST == "moist_euler"
+    @time if TRIXI_TEST == "all" || TRIXI_TEST == "moist_euler"
         include("test_2d_moist_euler.jl")
     end
 
-    @time if TRIXIATMO_TEST == "all" || TRIXIATMO_TEST == "spherical_advection"
+    @time if TRIXI_TEST == "all" || TRIXI_TEST == "spherical_advection"
         include("test_spherical_advection.jl")
     end
 
-    @time if TRIXIATMO_TEST == "all" || TRIXIATMO_TEST == "shallow_water_3d"
+    @time if TRIXI_TEST == "all" || TRIXI_TEST == "shallow_water_3d"
         include("test_3d_shallow_water.jl")
     end
 
-    @time if TRIXIATMO_TEST == "all" || TRIXIATMO_TEST == "shallow_water_2d_covariant"
+    @time if TRIXI_TEST == "all" || TRIXI_TEST == "shallow_water_2d_covariant"
         include("test_2d_shallow_water_covariant.jl")
     end
 
