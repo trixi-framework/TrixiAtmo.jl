b5ba80f28f9e6fa58ae833f988ca36e08b4cef76
Benedict Geihe <bgeihe@uni-koeln.de>
2025-05-15 10:44:40 +0200

    need to use Trixi initial condition

--- test/test_trixi_consistency.jl
+++ test/test_trixi_consistency.jl
@@ -14,20 +14,35 @@ isdir(outdir) && rm(outdir, recursive = true)
     trixi_elixir = joinpath(Trixi.examples_dir(), "tree_2d_dgsem",
                             "elixir_euler_warm_bubble.jl")
 
-    # Override maxiter and fluxes
+    # Override fluxes, polydeg, cfl, maxiters
     trixi_include(trixi_elixir;
                   volume_flux = Trixi.flux_chandrashekar,
                   surface_flux = Trixi.FluxLMARS(360.0),
+                  polydeg = 4,
+                  stepsize_callback = Trixi.StepsizeCallback(cfl = 0.2),
                   maxiters = maxiters)
 
     # Save errors
     errors_trixi = Main.analysis_callback(Main.sol)
 
-    # Dry air warm bubble test case in TrixiAtmo.jl
+    # Create an instance of Trixi's equations, just used for dispatch below
+    equations_trixi = Trixi.CompressibleEulerEquations2D(Main.warm_bubble_setup.gamma)
+    add_zeros = SVector(zero(eltype(Main.sol)), zero(eltype(Main.sol)))
+
+    # Redefine initial condition in Trixi.jl for CompressibleMoistEulerEquations2D
+    # Different formulae were used!
+    @inline function (setup::Main.WarmBubbleSetup)(x, t,
+                                                   ::CompressibleMoistEulerEquations2D)
+        ret_trixi = setup(x, t, equations_trixi)
+        return vcat(ret_trixi, add_zeros)
+    end
+
+    # Now use the elixir in TrixiAtmo
     elixir_atmo = joinpath(TrixiAtmo.examples_dir(), "elixir_moist_euler_dry_bubble.jl")
 
-    # Run with overrides
+    # Override initial condition, maxiters
     trixi_include(elixir_atmo,
+                  initial_condition = Main.warm_bubble_setup,
                   maxiters = maxiters)
 
     # Save errors
@@ -37,7 +52,7 @@ isdir(outdir) && rm(outdir, recursive = true)
         @test isapprox(error_trixi, error_atmo, rtol = 1e-12)
     end
     for (error_trixi, error_atmo) in zip(errors_trixi.linf, errors_atmo.linf)
-        @test isapprox(error_trixi, error_atmo, rtol = 3e-12)
+        @test isapprox(error_trixi, error_atmo, rtol = 6e-12)
     end
 end
 end
