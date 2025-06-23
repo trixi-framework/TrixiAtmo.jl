80e090f090eed2d3d9c1431afbca254ae54dcc62
Benedict Geihe <bgeihe@uni-koeln.de>
2025-06-23 19:40:55 +0200

    Revert "remove dry bubble for serial tests"
    This reverts commit fba9b732550f4e9e1245bd3f3189e9555b64ec11.

--- test/test_2d_moist_euler.jl
+++ test/test_2d_moist_euler.jl
@@ -7,6 +7,37 @@ include("test_trixiatmo.jl")
 
 EXAMPLES_DIR = TrixiAtmo.examples_dir()
 
+@trixiatmo_testset "elixir_moist_euler_dry_bubble" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                 "elixir_moist_euler_dry_bubble.jl"),
+                        l2=[
+                            1.300428671901329e-6,
+                            2.601090012108739e-5,
+                            0.0006660124630171347,
+                            0.008969786054960861,
+                            0.0,
+                            0.0
+                        ],
+                        linf=[
+                            1.0312042909910168e-5,
+                            0.00020625488871672815,
+                            0.006392107590872236,
+                            0.07612038028310053,
+                            0.0,
+                            0.0
+                        ],
+                        polydeg=3,
+                        tspan=(0.0, 0.1))
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    let
+        t = sol.t[end]
+        u_ode = sol.u[end]
+        du_ode = similar(u_ode)
+        @test (@allocated TrixiAtmo.Trixi.rhs!(du_ode, u_ode, semi, t)) < 100
+    end
+end
+
 @trixiatmo_testset "elixir_moist_euler_EC_bubble" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_moist_euler_EC_bubble.jl"),
                         l2=[
