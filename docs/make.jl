132b2c8f845b3c9430678a662cdbade7107823d8
Benedict Geihe <bgeihe@uni-koeln.de>
2025-06-23 15:51:56 +0200

    extend docs
    mostly parsing files in root folder

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Documenter
 using DocumenterInterLinks
+using TrixiAtmo
 
 # Fix for https://github.com/trixi-framework/Trixi.jl/issues/668
 if (get(ENV, "CI", nothing) != "true") &&
@@ -7,18 +8,48 @@ if (get(ENV, "CI", nothing) != "true") &&
     push!(LOAD_PATH, dirname(@__DIR__))
 end
 
-using TrixiAtmo
+# Define module-wide setups such that the respective modules are available in doctests
+DocMeta.setdocmeta!(TrixiAtmo, :DocTestSetup, :(using TrixiAtmo);
+                    recursive = true)
 
 # Provide external links to the Trixi.jl docs (project root and inventory file)
 links = InterLinks("Trixi" => ("https://trixi-framework.github.io/Trixi.jl/stable/",
                                "https://trixi-framework.github.io/Trixi.jl/stable/objects.inv"))
 
-DocMeta.setdocmeta!(TrixiAtmo, :DocTestSetup, :(using TrixiAtmo);
-                    recursive = true)
+# Copy list of authors to not need to synchronize it manually.
+# Since the authors header exists twice we create a unique identifier for the docs section.
+authors_text = read(joinpath(dirname(@__DIR__), "AUTHORS.md"), String)
+authors_text = replace(authors_text,
+                       "[LICENSE.md](LICENSE.md)" => "[License](@ref)",
+                       "# Authors" => "# [Authors](@id trixi_atmo_authors)")
+write(joinpath(@__DIR__, "src", "authors.md"), authors_text)
+
+# Copy contributing information to not need to synchronize it manually
+contributing_text = read(joinpath(dirname(@__DIR__), "CONTRIBUTING.md"), String)
+contributing_text = replace(contributing_text,
+                            "[LICENSE.md](LICENSE.md)" => "[License](@ref)",
+                            "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref trixi_atmo_authors)")
+write(joinpath(@__DIR__, "src", "contributing.md"), contributing_text)
+
+# Copy code of conduct to not need to synchronize it manually
+code_of_conduct_text = read(joinpath(dirname(@__DIR__), "CODE_OF_CONDUCT.md"), String)
+code_of_conduct_text = replace(code_of_conduct_text,
+                               "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref trixi_atmo_authors)")
+write(joinpath(@__DIR__, "src", "code_of_conduct.md"), code_of_conduct_text)
+
+# Copy contents form README to the starting page to not need to synchronize it manually
+readme_text = read(joinpath(dirname(@__DIR__), "README.md"), String)
+readme_text = replace(readme_text,
+                      "[LICENSE.md](LICENSE.md)" => "[License](@ref)",
+                      "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref trixi_atmo_authors)",
+                      "<p" => "```@raw html\n<p",
+                      "p>" => "p>\n```",
+                      r"\[comment\].*\n" => "")    # remove comments
+write(joinpath(@__DIR__, "src", "home.md"), readme_text)
 
 makedocs(;
          modules = [TrixiAtmo],
-         authors = "Benedict Geihe <bgeihe@uni-koeln.de>, Tristan Montoya <montoya.tristan@gmail.com, Hendrik Ranocha <hendrik.ranocha@uni-mainz.de>, Michael Schlottke-Lakemper <michael@sloede.com>",
+         authors = "Benedict Geihe <bgeihe@uni-koeln.de>, Tristan Montoya <montoya.tristan@gmail.com, Hendrik Ranocha <hendrik.ranocha@uni-mainz.de>, Andrés Rueda-Ramírez <am.rueda@upm.es>, Michael Schlottke-Lakemper <michael@sloede.com>",
          repo = Remotes.GitHub("trixi-framework", "TrixiAtmo.jl"),
          sitename = "TrixiAtmo.jl",
          format = Documenter.HTML(;
@@ -26,7 +57,12 @@ makedocs(;
                                   canonical = "https://trixi-framework.github.io/TrixiAtmo.jl",
                                   edit_link = "main",
                                   assets = String[],),
-         pages = ["Home" => "index.md"],
+         pages = ["Home" => "index.md",
+                  "Reference" => "reference.md",
+                  "Authors" => "authors.md",
+                  "Contributing" => "contributing.md",
+                  "Code of Conduct" => "code_of_conduct.md",
+                  "License" => "license.md"],
          plugins = [links],)
 
 deploydocs(;
---
+++ docs/src/reference.md
@@ -0,0 +1,9 @@
+```@meta
+CurrentModule = TrixiAtmo
+```
+
+# TrixiAtmo.jl API
+
+```@autodocs
+Modules = [TrixiAtmo]
+```
