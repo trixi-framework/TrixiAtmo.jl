7a09b6cd468ce268e1357b361b5e1129eae84e4d
Benedict Geihe <bgeihe@uni-koeln.de>
2025-06-24 12:19:22 +0200

    revert tolerance

--- test/test_trixi_consistency.jl
+++ test/test_trixi_consistency.jl
@@ -52,7 +52,7 @@ isdir(outdir) && rm(outdir, recursive = true)
         @test isapprox(error_trixi, error_atmo, rtol = 1e-12)
     end
     for (error_trixi, error_atmo) in zip(errors_trixi.linf, errors_atmo.linf)
-        @test isapprox(error_trixi, error_atmo, rtol = 5e-11)
+        @test isapprox(error_trixi, error_atmo, rtol = 1.1e-10)
     end
 end
 end
