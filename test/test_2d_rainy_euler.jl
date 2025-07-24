7496416f68ff3360935dccc28fa348bf6f6ad83a
Benedict Geihe <bgeihe@uni-koeln.de>
2025-07-24 12:22:04 +0200

    check CI_ON_MAC

--- test/test_2d_rainy_euler.jl
+++ test/test_2d_rainy_euler.jl
@@ -4,6 +4,8 @@ include("test_trixiatmo.jl")
 
 EXAMPLES_DIR = joinpath(EXAMPLES_DIR, "moist_euler")
 
+@info CI_ON_MAC
+
 @trixi_testset "convergence_test_rainy" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR,
                                  "convergence_test",
@@ -144,7 +146,7 @@ end
                             7.049541733574276e-6,
                             0.008742438145986853
                         ],
-                        CI_ON_MAC ? rtol=4e-7 : 1e-13,
+                        CI_ON_MAC ? rtol=4e-7 : rtol=1e-20,
                         initial_refinement_level=4,
                         tspan=(0.0, 10.0))
     # Ensure that we do not have excessive memory allocations
