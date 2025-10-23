161012c64334b03e58ce39ba57ed2c81a445a4ea
Marco Artiano <artianomarco97@gmail.com>
2025-10-23 16:56:35 +0200

    introduce atol

--- test/test_3d_euler_energy.jl
+++ test/test_3d_euler_energy.jl
@@ -20,7 +20,7 @@ include("test_trixiatmo.jl")
                             0.022711918877967086,
                             0.9494724421529099,
                             0.3119894564151764
-                        ], tspan=(0.0, 0.01), tol=1e-15)
+                        ], tspan=(0.0, 0.01), tol=1e-15, atol=1e-8)
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
     @test_allocations(TrixiAtmo.Trixi.rhs!, semi, sol, 100)
