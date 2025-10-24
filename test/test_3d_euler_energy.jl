4859f1190696d755cdd8bae6c1383b703b4f927e
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2025-10-24 10:06:29 +0200

    Add `CompressibleEulerEnergyEquationsWithGravity3D` (#121)
    * add CompressibleEulerEnergyEquationsWithGravity3D
    
    * fix space
    
    * fmt
    
    * add tests to runtests.jl
    
    * fix coverage
    
    * fix test values for MacOS
    
    * introduce atol
    
    * Apply suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

---
+++ test/test_3d_euler_energy.jl
@@ -0,0 +1,29 @@
+module TestExamples3DEulerEnergy
+
+include("test_trixiatmo.jl")
+
+@trixi_testset "elixir_euler_energy_baroclinic_instability" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                 "elixir_euler_energy_baroclinic_instability.jl"),
+                        l2=[
+                            6.552421954615372e-7,
+                            0.00019731220715342904,
+                            0.00042455589284480093,
+                            0.0001871831573805277,
+                            0.06704750535405761,
+                            0.0409097168117604
+                        ],
+                        linf=[
+                            4.428967270930784e-6,
+                            0.024276204894145392,
+                            0.045702372863804186,
+                            0.022711918877967086,
+                            0.9494724421529099,
+                            0.3119894564151764
+                        ], tspan=(0.0, 0.01), tol=1e-15, atol=1e-8)
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    @test_allocations(TrixiAtmo.Trixi.rhs!, semi, sol, 100)
+end
+
+end
