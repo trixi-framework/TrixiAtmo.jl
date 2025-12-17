6942d747f74826ad132b0732e6efe1f8f88acaf9
Benedict Geihe <bgeihe@uni-koeln.de>
2025-12-17 16:53:22 +0100

    nope

--- test/test_mpi.jl
+++ test/test_mpi.jl
@@ -12,26 +12,24 @@ Trixi.MPI.Barrier(Trixi.mpi_comm())
 @testset verbose=true showtiming=true "MPI tests" begin
 #! format: noindent
 
-@trixi_testset "elixir_moist_euler_moist_bubble" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_moist_euler_moist_bubble.jl"),
+@trixi_testset "elixir_moist_euler_dry_bubble" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_moist_euler_dry_bubble.jl"),
                         l2=[
-                            7.3515680983123215e-6,
-                            1.1067008939664827e-7,
-                            0.0006971968385493199,
-                            1.715939603224655,
-                            8.832720252288771e-7,
-                            1.025736269959355e-6
+                            9.104437114458848e-7,
+                            1.8210536975490044e-5,
+                            0.0004707887343135412,
+                            0.0063400898518523935,
+                            0.0,
+                            0.0
                         ],
                         linf=[
-                            8.056395313560394e-5,
-                            1.1981461033088162e-6,
-                            0.0058959697735631155,
-                            19.248694115842227,
-                            1.0043632092967755e-5,
-                            1.1439573103299433e-5
+                            1.0258941581242631e-5,
+                            0.00020520634691933992,
+                            0.006392782691233334,
+                            0.07637640493339859,
+                            0.0,
+                            0.0
                         ],
-                        polydeg=3,
-                        cells_per_dimension=(16, 8),
                         tspan=(0.0, 0.1))
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
