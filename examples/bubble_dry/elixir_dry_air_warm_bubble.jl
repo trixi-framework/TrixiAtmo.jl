1399c9ccb13f82405a7786e788e0feaa05f1bced
Benedict Geihe <bgeihe@uni-koeln.de>
2026-01-21 11:06:59 +0100

    cleanup

--- examples/bubble_dry/elixir_dry_air_warm_bubble.jl
+++ examples/bubble_dry/elixir_dry_air_warm_bubble.jl
@@ -4,21 +4,35 @@ using TrixiAtmo
 using TrixiAtmo: initial_condition_dry_air_warm_bubble_generator, source_terms_gravity_cartZ_generator
 using Plots
 
+
+###############################################################################
+# Parameters
+
 RealType = Float64
 n_dim = 2
 n_passive = 0
 
-parameters = Parameters{RealType}()
+parameters = Parameters{RealType}(;
+    earth_gravitational_acceleration = 9.81
+)
+
+
+###############################################################################
+# Thermodynamics
 
 td_multi = IdealGasesAndLiquids(; parameters,
     n_gas = 2, n_condens = 1, n_precip = 1)
-td_single = IdealGas(; parameters)
 
 td_totE = TotalEnergy(td_multi)
 
-td_potT = PotentialTemperature(td_single)
+# td_single = IdealGas(; parameters)
+# td_potT = PotentialTemperature(td_single)
 
 microphysics = MicrophysicsRelaxation{RealType}()
+
+
+###############################################################################
+# Equations
 
 equations = CompressibleEulerAtmo{n_dim}(; parameters = parameters,
                                         thermodynamic_state = td_multi,
@@ -26,6 +40,30 @@ equations = CompressibleEulerAtmo{n_dim}(; parameters = parameters,
                                         #thermodynamic_state = td_single,
                                         #thermodynamic_equation = td_potT,
                                         microphysics = microphysics)
+
+
+###############################################################################
+# Initial and boundary conditions
+
+initial_condition_reference = initial_condition_dry_air_warm_bubble_generator(
+    parameters;
+    perturbation_center_x = 500.0,
+    perturbation_center_z = 260.0,
+    perturbation_radius = 250.0
+)
+initial_condition = transform_initial_condition(initial_condition_reference, equations)
+
+
+###############################################################################
+# Source terms
+
+source_terms_gravity_reference = source_terms_gravity_cartZ_generator(Val(2);
+    g = parameters.earth_gravitational_acceleration)
+source_terms_gravity = transform_source_terms(source_terms_gravity_reference, equations)
+
+
+###############################################################################
+# Solver
 
 surface_flux = flux_lax_friedrichs
 #surface_flux = FluxLMARS(340.0)
@@ -44,17 +82,6 @@ mesh = P4estMesh(trees_per_dimension, polydeg = 3,
                  coordinates_min = coordinates_min, coordinates_max = coordinates_max,
                  periodicity = (true, false), initial_refinement_level = 2)
 
-initial_condition_reference = initial_condition_dry_air_warm_bubble_generator(
-    parameters;
-    perturbation_center_x = 500.0,
-    perturbation_center_z = 260.0,
-    perturbation_radius = 250.0
-)
-initial_condition = transform_initial_condition(initial_condition_reference, equations)
-
-source_terms_gravity_reference = source_terms_gravity_cartZ_generator(Val(2);
-    g = parameters.earth_gravitational_acceleration)
-source_terms_gravity = transform_source_terms(source_terms_gravity_reference, equations)
 
 boundary_conditions = Dict(:y_neg => boundary_condition_slip_wall,
                            :y_pos => boundary_condition_slip_wall)
