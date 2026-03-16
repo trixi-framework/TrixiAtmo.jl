c621a12752138effb8a3431bad349f7bdc6e461c
Philipp Baasch <145576657+PhilBaa@users.noreply.github.com>
2026-03-16 18:46:56 +0100

    Covariant solver in DGMulti (#140)
    * Add covariant solver for spherical advection and shallow water equations in DGMulti
    
    * Fixed convergence issues + formatting
    
    * Remove redundant auxiliary metric computations in init_auxiliary_node_variables!
    
    * Refactor init_auxiliary_node_variables! function signature by removing unused parameters
    
    * Update barotropic instability example
    
    * Moved 3D MeshData for 2D RefElem to TrixiAtmo (https://github.com/jlchan/StartUpDG.jl/pull/211)
    
    * Minor fix
    
    * Improved analysis by using area_element
    
    * Added Tests
    
    * Added tests for real
    
    * Formatting
    
    * Removed godunov flux
    
    ---------
    
    Co-authored-by: Tristan Montoya <montoya.tristan@gmail.com>

--- test/test_spherical_advection.jl
+++ test/test_spherical_advection.jl
@@ -128,4 +128,22 @@ end
     @test_allocations(TrixiAtmo.Trixi.rhs!, semi, sol, 100)
 end
 
+@trixi_testset "Spherical advection on icosahedral grid, covariant weak form, LLF surface flux" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "advection/covariant",
+                                 "elixir_tri_icosahedron.jl"),
+                        l2=[
+                            0.00037441291907931983,
+                            1.0851024650858329e-13,
+                            1.0760196451424737e-13
+                        ],
+                        linf=[
+                            0.010071157495758598,
+                            5.18004530328936e-13,
+                            5.133704062982442e-13
+                        ])
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    @test_allocations(TrixiAtmo.Trixi.rhs!, semi, sol, 100)
+end
+
 end # module
