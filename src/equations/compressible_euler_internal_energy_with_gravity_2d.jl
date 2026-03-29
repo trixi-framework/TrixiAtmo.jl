e861faec63aad0670bb3e5ccc4cee0165677f7be
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2026-03-29 15:40:20 +0200

    Update src/equations/compressible_euler_internal_energy_with_gravity_2d.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/equations/compressible_euler_internal_energy_with_gravity_2d.jl
+++ src/equations/compressible_euler_internal_energy_with_gravity_2d.jl
@@ -9,7 +9,7 @@
 CompressibleEulerInternalEnergyEquationsWithGravity2D(; c_p, c_v, gravity) 
 
 The compressible Euler equations with gravity
-math
+```math
 \frac{\partial}{\partial t}
 \begin{pmatrix}
 \rho \\ \rho v_1 \\ \rho v_2 \\ \rho e_{\text{internal}}
@@ -32,6 +32,7 @@ math
 \begin{pmatrix}
 0 \\ - \rho \frac{\partial}{\partial x} \phi \\ - \rho \frac{\partial}{\partial y} \phi \\
 \end{pmatrix}
+```
 for an ideal gas with ratio of specific heats gamma in two space dimensions. Here, `\rho is the density, v_1, v_2 are the velocities, e_{\text{internal}} is the specific internal energy, phi is the gravitational potential, and
 math
 p = (\gamma - 1) \rho e_internal
