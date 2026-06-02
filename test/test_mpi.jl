1c3d5f0c8fa4e4d2fda8dda3a7913df0d4d165e6
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2026-06-02 18:10:48 +0200

    Added MPI test

--- test/test_mpi.jl
+++ test/test_mpi.jl
@@ -35,6 +35,40 @@ Trixi.MPI.Barrier(Trixi.mpi_comm())
     # (e.g., from type instabilities)
     @test_allocations(TrixiAtmo.Trixi.rhs!, semi, sol, 1000)
 end
+
+@trixi_testset "elixir_potential_temperature_vortex_shedding with Sleve" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "euler", "dry_air", "global_circulation",
+                                 "elixir_potential_temperature_vortex_shedding.jl"),
+                        l2=[
+                            0.0001056726895239208,
+                            0.03612902927134097,
+                            0.036128771203962594,
+                            0.047764018298670316,
+                            0.03181247660774069,
+                            0.5931596702203235
+                        ],
+                        linf=[
+                            0.0011918734879285964,
+                            0.15502631302821748,
+                            0.15502631302823136,
+                            0.25776642079474976,
+                            0.3442945234858712,
+                            18.51483958443782
+                        ],
+                        rtol=1e-9,
+                        tspan=(0.0, 0.0001 * SECONDS_PER_DAY),
+                        trees_per_cube_face=(2, 2), adapt_vertical_grid=Sleve(0.7, 0.8))
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    @test_allocations(TrixiAtmo.Trixi.rhs!, semi, sol, 100)
+    # Check partitioning
+    nelems_min = copy(nelements(solver, semi.cache))
+    nelems_max = copy(nelements(solver, semi.cache))
+    Trixi.MPI.Allreduce!(Ref(nelems_min), Base.min, Trixi.mpi_comm())[]
+    Trixi.MPI.Allreduce!(Ref(nelems_max), Base.max, Trixi.mpi_comm())[]
+    @assert nelems_min == 26
+    @assert neleme_max == 28
+end
 end
 
 # Clean up afterwards: delete Trixi.jl output directory
