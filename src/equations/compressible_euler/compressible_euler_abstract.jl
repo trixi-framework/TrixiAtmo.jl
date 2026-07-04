75ec91955e467712180210ec239f6d63fe042e80
Benedict Geihe <bgeihe@uni-koeln.de>
2026-07-04 11:21:00 +0200

    format

--- src/equations/compressible_euler/compressible_euler_abstract.jl
+++ src/equations/compressible_euler/compressible_euler_abstract.jl
@@ -6,15 +6,14 @@
 abstract type AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE} <:
               AbstractCompressibleEulerEquations{NDIMS, NVARS} end
 
-
 @muladd begin
 #! format: noindent
 
 @inline nvariables_active(::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE}) where
-    {NDIMS, NVARS, NPASSIVE} = NVARS - NPASSIVE
+{NDIMS, NVARS, NPASSIVE} = NVARS - NPASSIVE
 
 @inline nvariables_passive(::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE}) where
-    {NDIMS, NVARS, NPASSIVE} = NPASSIVE
+{NDIMS, NVARS, NPASSIVE} = NPASSIVE
 
 # TODO total or dry air?
 #@inline function density(u, ::AbstractCompressibleEulerAtmo{NDIMS}) where {NDIMS}
@@ -22,29 +21,40 @@ abstract type AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE} <:
 #end
 
 # Momentum densities come first
-@inline function velocity(u, equations::AbstractCompressibleEulerAtmo{NDIMS}) where {NDIMS}
+@inline function velocity(u,
+                          equations::AbstractCompressibleEulerAtmo{NDIMS}) where {NDIMS}
     rho = density_total(u, equations)
-    return SVector(ntuple(@inline(v->u[v]/rho), Val(NDIMS)))
+    return SVector(ntuple(@inline(v->u[v] / rho), Val(NDIMS)))
 end
 
 # thermodynamic variable next (NDIMS+1)
 
 # dry air density next
 @inline function density_dry(u, ::AbstractCompressibleEulerAtmo{NDIMS}) where {NDIMS}
-    return u[NDIMS+2]
+    return u[NDIMS + 2]
 end
 
 # all species, besides passive, are included when computing total density 
 @inline function density_total(u,
-    ::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE}) where {NDIMS, NVARS, NPASSIVE}
-    return sum(u[SVector{NVARS-NPASSIVE-NDIMS-1}(NDIMS+2:NVARS-NPASSIVE)])
+                               ::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE}) where {
+                                                                                               NDIMS,
+                                                                                               NVARS,
+                                                                                               NPASSIVE
+                                                                                               }
+    return sum(u[SVector{NVARS - NPASSIVE - NDIMS - 1}((NDIMS + 2):(NVARS - NPASSIVE))])
 end
 
 @inline function prim2density_total(prim,
-    ::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE}) where {NDIMS, NVARS, NPASSIVE}
+                                    ::AbstractCompressibleEulerAtmo{NDIMS, NVARS,
+                                                                    NPASSIVE}) where {
+                                                                                      NDIMS,
+                                                                                      NVARS,
+                                                                                      NPASSIVE
+                                                                                      }
     # fraction of dry air density
-    r_d = 1 - sum(prim[SVector{NVARS-NPASSIVE-NDIMS-2}(NDIMS+3:NVARS-NPASSIVE)])
-    return prim[NDIMS+2] / r_d
+    r_d = 1 -
+          sum(prim[SVector{NVARS - NPASSIVE - NDIMS - 2}((NDIMS + 3):(NVARS - NPASSIVE))])
+    return prim[NDIMS + 2] / r_d
 end
 
 # Calculate kinetic energy for a conservative state
@@ -62,21 +72,25 @@ end
 # - in the form (rho_d, u, v, [w, ] p, rho_v, ...)
 #   in this case the number of variables has to match those of the equations
 # Additional tracer can be given as initial_tracer
-function transform_initial_condition(
-    initial_condition,
-    ::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE};
-    initial_tracers = zeros(SVector{NPASSIVE, Int64})) where {NDIMS, NVARS, NPASSIVE}
-    
+function transform_initial_condition(initial_condition,
+                                     ::AbstractCompressibleEulerAtmo{NDIMS, NVARS,
+                                                                     NPASSIVE};
+                                     initial_tracers = zeros(SVector{NPASSIVE, Int64})) where {
+                                                                                               NDIMS,
+                                                                                               NVARS,
+                                                                                               NPASSIVE
+                                                                                               }
     @inline function initial_condition_transformed(x, t,
-        equations::AbstractCompressibleEulerAtmo{NDIMS, NVARS, NPASSIVE}) where
-        {NDIMS, NVARS, NPASSIVE}
+                                                   equations::AbstractCompressibleEulerAtmo{NDIMS,
+                                                                                            NVARS,
+                                                                                            NPASSIVE}) where
+                     {NDIMS, NVARS, NPASSIVE}
         prim = initial_condition(x, t, equations)
-        len = min(length(prim), NVARS-NPASSIVE)
-        prim_transformed = SVector(prim[SVector{NDIMS+1}(2:NDIMS+2)]...,
+        len = min(length(prim), NVARS - NPASSIVE)
+        prim_transformed = SVector(prim[SVector{NDIMS + 1}(2:(NDIMS + 2))]...,
                                    prim[1],
-                                   prim[SVector{len-NDIMS-2}(
-                                        NDIMS+3:len)]...,
-                                   ntuple(i->0, Val(NVARS-NPASSIVE-len))...,
+                                   prim[SVector{len - NDIMS - 2}((NDIMS + 3):len)]...,
+                                   ntuple(i -> 0, Val(NVARS - NPASSIVE - len))...,
                                    initial_tracers...)
         return prim2cons(prim_transformed, equations)
     end
@@ -84,13 +98,15 @@ function transform_initial_condition(
 end
 
 # Transform source terms
-# By convention refers to conservative variables
-# in the form (rho, rho u, rho v, [rho w, ] rho X) with X being the thermodynamic quantitiy
+# By convention refers to conservative variables in the form
+# (rho u, rho v, [rho w, ] rho X, rho, ... ) with X being the thermodynamic quantitiy
+# TODO no rearrange?
 function transform_source_terms_sum(source_terms::Tuple,
                                     ::AbstractCompressibleEulerAtmo)
     function source_terms_transformed(u, x, t,
-        equations::AbstractCompressibleEulerAtmo{NDIMS, NVARS}) where
-        {NDIMS, NVARS}
+                                      equations::AbstractCompressibleEulerAtmo{NDIMS,
+                                                                               NVARS}) where
+             {NDIMS, NVARS}
         # rearrange to match with convention
         #u_ref = SVector(u[NDIMS+2],
         #                u[SVector{NDIMS+1}(1:NDIMS+1)]...,
@@ -99,7 +115,7 @@ function transform_source_terms_sum(source_terms::Tuple,
         # evaluate and add source terms
         source_ref = mapreduce(+, source_terms) do f
             ret = f(u, x, t, equations)
-            SVector(ret..., ntuple(i->0, Val(NVARS-length(ret)))...)
+            SVector(ret..., ntuple(i -> 0, Val(NVARS - length(ret)))...)
         end
 
         return source_ref
@@ -111,11 +127,9 @@ function transform_source_terms_sum(source_terms::Tuple,
     return source_terms_transformed
 end
 
-transform_source_terms(source_terms, equations::AbstractCompressibleEulerAtmo) =
-    transform_source_terms_sum((source_terms,), equations)
-
+transform_source_terms(source_terms, equations::AbstractCompressibleEulerAtmo) = transform_source_terms_sum((source_terms,),
+                                                                                                            equations)
 end # @muladd
-
 
 # Include concrete types of equations
 include("compressible_euler_atmo.jl")
