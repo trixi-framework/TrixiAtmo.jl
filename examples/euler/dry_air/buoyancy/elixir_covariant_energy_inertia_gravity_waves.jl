f2117c97d503066732fffc69dac83df2d6d22af0
Philipp Baasch <145576657+PhilBaa@users.noreply.github.com>
2026-08-17 13:07:18 +0200

    Update examples/euler/dry_air/buoyancy/elixir_covariant_energy_inertia_gravity_waves.jl
    Co-authored-by: Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>

--- examples/euler/dry_air/buoyancy/elixir_covariant_energy_inertia_gravity_waves.jl
+++ examples/euler/dry_air/buoyancy/elixir_covariant_energy_inertia_gravity_waves.jl
@@ -107,9 +107,6 @@ analysis_callback = AnalysisCallback(semi, interval = 100,
 save_solution = SaveSolutionCallback(interval = 100,
                                      solution_variables = contravariant_cons2global_prim)
 
-# The StepsizeCallback handles the re-calculation of the maximum Δt after each time step
-stepsize_callback = StepsizeCallback(cfl = 0.7)
-
 # Create a CallbackSet to collect all callbacks such that they can be passed to the ODE 
 # solver
 callbacks = CallbackSet(summary_callback, analysis_callback, save_solution,
